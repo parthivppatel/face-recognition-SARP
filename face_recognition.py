28155538381e4d1bd2f1a335921715d89d0a250c
Sarma-savitriya <sarma@savitriya.com>
2023-07-15 14:05:45 +0530

    UPDATE - add instructions in the README.MD file

--- face_recognition.py
+++ face_recognition.py
@@ -34,7 +34,7 @@ while True:
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
         id, confidence = recognizer.predict(gray[y : y + h, x : x + w])
 
-        # If confidence is less them 100 ==> "0" : perfect match
+        # If confidence is less than 100 ==> "0" : perfect match
         if confidence < 100:
             id = names[id]
             confidence = "  {0}%".format(round(100 - confidence))

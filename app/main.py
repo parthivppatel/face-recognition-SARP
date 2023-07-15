b168d4089e942937355e7a86cbebc8ff9956d0f6
Ravin <rk@savitriya.com>
2023-07-15 13:35:02 +0530

    - create api to store the capture data

--- app/main.py
+++ app/main.py
@@ -1,8 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends,HTTPException
+from sqlalchemy.orm import Session
+import databaseutils
+from schemas.face import FaceData
+from models.face import FaceCapture
+from datetime import datetime
 
-app = FastAPI()
+app=FastAPI()
 
+@app.get("/", tags=["Default"])
+def read_root():
+    return {"Hello": "World"}
 
-@app.get("/")
-async def read_root():
-    return {"message": "Hello, FastAPI!"}
+@app.post("/face-data")
+def captureFace(FaceData:FaceData,db: Session = Depends(databaseutils.get_db)):
+    
+    try:
+        face=FaceCapture(
+            device_id=FaceData.device_id,
+            face_id =FaceData.face_id,
+            date=datetime.now()
+        )
+        db.add(face)
+        db.commit()
+        
+    except:
+        raise HTTPException(500,"Something Went Wrong")
+
+    return "Data Added Succesfully"
---
+++ app/models/face.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql.sqltypes import DateTime
+from database import Base
+
+
+class FaceCapture(Base):
+    __tablename__="face_data"    
+    
+    id = Column(Integer(), primary_key=True, index=True)
+    device_id = Column(String(255), index=True, unique=False,nullable=False)
+    face_id=Column(String(255),nullable=True)
+    date=Column(DateTime,nullable=False)
